7fce513c567c87332506bb8b6f565820e3554ffd
darginid <dargowork@gmail.com>
2026-02-07 16:47:39 +0300

    add JavaScript logic

--- app.js
+++ app.js
@@ -1,1 +1,51 @@
-console.log("Hello World!");
+function getComputerChoice() {
+  const random = Math.floor(Math.random() * (2 - 0 + 1));
+  if (random === 0) {
+    return "rock";
+  } else if (random === 1) {
+    return "scissors";
+  }
+
+  return "paper";
+}
+
+function getHumanChoice() {
+  const result = prompt();
+  return result;
+}
+
+function playGame() {
+  let humanScore = 0;
+  let computerScore = 0;
+
+  function playRound(humanChoice, computerChoice) {
+    if (humanChoice === computerChoice) {
+      console.log("Draw");
+    } else if (
+      (humanChoice === "rock" && computerChoice === "scissors") ||
+      (humanChoice === "scissors" && computerChoice === "paper") ||
+      (humanChoice === "paper" && computerChoice === "rock")
+    ) {
+      humanScore += 1;
+      console.log("You Win");
+    } else {
+      computerScore += 1;
+      console.log("Computer Win");
+    }
+  }
+
+  for (let i = 1; i <= 5; i++) {
+    const humanSelection = getHumanChoice().toLowerCase();
+    const computerSelection = getComputerChoice();
+    playRound(humanSelection, computerSelection);
+  }
+  if (humanScore > computerScore) {
+    console.log("Game Win You");
+  } else if (humanScore < computerScore) {
+    console.log("Game Win Computer");
+  } else {
+    console.log("Draw in Game");
+  }
+}
+
+playGame();
